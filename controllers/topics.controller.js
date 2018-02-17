9d77bd23f2472e58e17bd7e8979945fdf06743d0
tomathon <tomathonmiller@gmail.com>
2018-02-17 12:19:27 +0000

    Add getArticlesByTopicId function and error handling

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,12 +1,34 @@
 const mongoose = require('mongoose');
 const Topics = require('../models/topics');
+const Articles = require('../models/articles');
 
-getAllTopics = (req, res, next) => {
+const getAllTopics = (req, res, next) => {
   Topics.find()
     .then((topics) => {
-      res.send(topics);
+      res.status(200).json({topics});
     })
-    .catch(console.error);
+    .catch(err => {
+      return next({
+        status: 404,
+        message: "404, page not found"
+      })
+    });
 }
 
-module.exports = getAllTopics;
+const getArticlesByTopicId = (req, res, next) => {
+  Topics.find({_id : req.params.topic_id})
+    .then(topic => {
+      return Articles.find({belongs_to : topic[0].slug})
+    })
+    .then(articles => {
+      res.status(200).json({articles});
+    })
+    .catch(err => {
+      return next({
+        status: 404,
+        message: "404, page not found"
+      })
+    });
+}
+
+module.exports = { getAllTopics, getArticlesByTopicId };
--- routes/topics.js
+++ routes/topics.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const topicsRouter = require('express').Router();
-const getAllTopics = require('../controllers/topics.controller');
+const { getAllTopics, getArticlesByTopicId } = require('../controllers/topics.controller');
 
 topicsRouter.route('/')
   .get(getAllTopics);
-  
+
+topicsRouter.route('/:topic_id/articles')
+  .get(getArticlesByTopicId);
+
 module.exports = topicsRouter;
