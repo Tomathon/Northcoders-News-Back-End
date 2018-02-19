171c3718610ceb2f7e5462af0725095eda89cdbe
tomathon <tomathonmiller@gmail.com>
2018-02-19 20:52:50 +0000

    Add updateCommentVote function

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -13,4 +13,24 @@ const deleteComment = (req, res, next) => {
   }
 }
 
-module.exports = deleteComment;
+const updateCommentVote = (req, res, next) => {
+  const id = req.params.comment_id;
+  const query = req.query.vote
+  if (!id.match(/^[0-9a-f]{24}$/)) next({message: `Comment ${id} does not exist, please enter a valid comment id`, status: 400})
+  else if (query !== 'up' && query !== 'down') next({message: "Please provide a query in the format vote=up or vote=down", status: 400})
+  else {
+    Comments.findByIdAndUpdate(id)
+    .then(comment => {
+      let votes = comment.votes
+      if (query === 'up') votes += 1
+      if (query === 'down') votes -= 1
+      return Comments.update({_id: id}, {votes: votes})
+      })
+      .then(updatedComment => {
+        res.status(201).send(`Thanks for your vote on Comment ${id}`)
+      })
+      .catch(next)
+  }
+}
+
+module.exports = { deleteComment, updateCommentVote };
--- routes/comments.js
+++ routes/comments.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const commentsRouter = require('express').Router();
-const deleteComment = require('../controllers/comments.controller');
+const { deleteComment, updateCommentVote } = require('../controllers/comments.controller');
 
 commentsRouter.route('/:comment_id')
   .delete(deleteComment)
+  .put(updateCommentVote)
 
 module.exports = commentsRouter;
