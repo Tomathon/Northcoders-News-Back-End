9a84d23e42d949a5176737cdb143dda199dae428
tomathon <tomathonmiller@gmail.com>
2018-02-20 20:28:41 +0000

    Update error handling in topics.controller

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -2,20 +2,17 @@ const mongoose = require('mongoose');
 const Topics = require('../models/topics');
 const Articles = require('../models/articles');
 
-const getAllTopics = (req, res, next) => {
+const getAllTopics = (req, res) => {
   Topics.find()
     .then((topics) => {
       res.status(200).json({topics});
     })
     .catch(err => {
-      return next({
-        status: 404,
-        message: "404, page not found"
-      })
+      res.status(500).json({"message": "Sorry, something went wrong"})
     });
 }
 
-const getArticlesByTopicId = (req, res, next) => {
+const getArticlesByTopicId = (req, res) => {
   Topics.find({_id : req.params.topic_id})
     .then(topic => {
       return Articles.find({belongs_to : topic[0].slug})
@@ -24,10 +21,7 @@ const getArticlesByTopicId = (req, res, next) => {
       res.status(200).json({articles});
     })
     .catch(err => {
-      return next({
-        status: 404,
-        message: "404, page not found"
-      })
+      res.status(500).json({"message": "Sorry, something went wrong"})
     });
 }
 
