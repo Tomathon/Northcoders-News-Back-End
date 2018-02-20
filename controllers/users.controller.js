fa494701199dabf45e5d426d1f157dee30dbb038
tomathon <tomathonmiller@gmail.com>
2018-02-20 20:26:20 +0000

    Update error handling in users.controller

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 const Users = require('../models/users');
 
-const getUser = (req, res, next) => {
+const getUser = (req, res) => {
   Users.findOne({ username: req.params.username })
     .then(user => {
-      if (user === null) next({ message: 'Username does not exist, please ensure the username is valid', status: 400})
+      if (user === null) res.status(400).json({"message": "Username does not exist, please ensure the username is valid"})
       else res.status(200).json({user})
     })
-    .catch(next);
+    .catch(err => {
+      res.status(500).json({"message": "Sorry, something went wrong"})
+    });
 }
 
 module.exports = getUser;
