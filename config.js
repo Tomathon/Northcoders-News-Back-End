68bbe4b6464d8baf5fedf7346efb3bd7e6cfb331
tomathon <tomathonmiller@gmail.com>
2018-02-26 20:43:49 +0000

    Added config changes for deployment on Heroku

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   DB: {
     test: 'mongodb://localhost/northcoders-news-api-test',
-    dev: 'mongodb://localhost/northcoders-news-api'
+    dev: process.env.MONGODB_URI || 'mongodb://localhost/northcoders-news-api'
   },
   PORT: {
     test: 3090,
