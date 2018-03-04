6dc90b5ba46a25c6cea8c0febd03f7e10ec573d0
tomathon <tomathonmiller@gmail.com>
2018-03-04 16:13:12 +0000

    Update voting function

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Comments = require('../models/comments')
+const Articles = require('../models/articles')
 
 const deleteComment = (req, res) => {
   const id = req.params.comment_id;
@@ -21,15 +22,13 @@ const updateCommentVote = (req, res) => {
   if (!id.match(/^[0-9a-f]{24}$/)) res.status(400).json({"message": `Comment ${id} does not exist, please enter a valid comment id`})
   else if (query !== 'up' && query !== 'down') res.status(400).json({"message": "Please provide a query in the format vote=up or vote=down"})
   else {
-    Comments.findByIdAndUpdate(id)
-      .then(comment => {
-        let votes = comment.votes
-        if (query === 'up') votes += 1
-        if (query === 'down') votes -= 1
-        return Comments.update({_id: id}, {votes: votes})
-        })
-      .then(updatedComment => {
-        res.status(201).json({"message":`Thanks for your vote on Comment ${id}`})
+    let count = 0;
+    if (query === 'up') count++;
+    if (query === 'down') count--;
+    return Comments.findByIdAndUpdate({_id: req.params.comment_id}, { $inc: {"votes": count}})
+      .then(results => {
+        results.votes += count;
+        res.status(200).json({results});
       })
       .catch(err => {
         res.status(500).json({"message":"Sorry, something went wrong"})
